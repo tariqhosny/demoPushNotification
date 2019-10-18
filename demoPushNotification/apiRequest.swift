52eae1dd10c4422d98743f2f6a90e9e417164f32
tariqhosny <tariqhosny@gmail.com>
2019-10-18 19:45:50 +0200

    first commit

---
+++ demoPushNotification/apiRequest.swift
@@ -0,0 +1,13 @@
+//
+//  apiRequest.swift
+//  demoPushNotification
+//
+//  Created by Tariq on 10/18/19.
+//  Copyright © 2019 Tariq. All rights reserved.
+//
+
+import UIKit
+
+class apiRequest: NSObject {
+
+}
